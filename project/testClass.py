d2d4b948fbfcd13ac62bdc90bd3263e4cb06afce
david95j2 <glory20220j@gmail.com>
2021-12-01 21:16:27 +0900

    test Calculator class

--- project/testClass.py
+++ project/testClass.py
@@ -2,21 +2,23 @@ import unittest
 
 from main import Calculator
 
+c = Calculator()
+
 class TestCalculator(unittest.TestCase):
 
   def test_add(self) :
     self.assertIsInstance(2, int)
-    print(Calculator.add(2,3))
+    c.add(2,3)
 
   def test_subtract(self) :
     self.assertTrue(3 > 2)
-    print(Calculator.subtract(2,3))
+    c.sub(2,3)
 
   def test_mul(self) :
     self.assertIsInstance(2, int)
     self.assertIsInstance(3, int)
-    print(Calculator.mul(2,3))
+    c.mul(2,3)
 
   def test_div(self) :
     self.assertTrue(3 > 0)
-    print(Calculator.div(2,3))
+    c.div(2,3)
